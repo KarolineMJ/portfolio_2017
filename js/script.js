89bdb68496cecc892431f99bd14997d008430887
KarolineMJ <Karoline_MJ@hotmail.com>
2018-01-04 20:57:06 +0100

    some

---
+++ js/script.js
@@ -0,0 +1,32 @@
+let scrollPos = window.scrollY;
+
+let navBar = document.getElementById('navBar');
+// selects the element by Id
+
+function addClassOnScroll(){
+	navBar.classList.add("scrolled");
+}
+
+function removeClassOnScroll(){
+	navBar.classList.remove("scrolled");
+}
+
+
+window.addEventListener('scroll', function(){
+	scrollPos = window.scrollY;
+
+	if(scrollPos > 60){
+		addClassOnScroll();
+	}
+	else{
+		removeClassOnScroll();
+	}
+	console.log(scrollpos);
+
+});
+
+function load(){
+	window.open("welcome.html", "_self");
+}
+
+
